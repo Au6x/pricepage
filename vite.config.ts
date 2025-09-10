f961e2ffd9eed35eab445f8ee7cb93bf1e396491
Raymond <mrau6x@gmail.com>
2025-09-10 14:25:23 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // 👇 This is critical for GitHub Pages
-  base: "/pricepage/",
+  base: "/raypricepage",
 
   server: {
     host: "::",
